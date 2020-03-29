6f08cf069910be8c3b13bacf867c7478a7cc1081
Anton Fedyashov <anton.fedyashov@gmail.com>
2020-03-29 16:49:17 +0300

    Test

--- lazarusGUI/source.pas
+++ lazarusGUI/source.pas
@@ -599,7 +599,7 @@ end;
 
 procedure TMainForm.cbHash1EditingDone(Sender: TObject);
 begin
-     Memo1.Text := cbHash1.Text + ' || ' + cbHash1.SelText + ' || ' + cbHash1.Items.ValueFromIndex[cbHash1.Items.IndexOf(Hash)];
+     Memo1.Text := cbHash1.Text + ' || ' + cbHash1.SelText;
 end;
 
 procedure TMainForm.dtFromEditingDone(Sender: TObject);
