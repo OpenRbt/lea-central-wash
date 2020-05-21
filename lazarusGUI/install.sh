c72831fb27ef9ba8f3ba5654f185e5c895cf4f8d
Roman Fedyashov <roman.fedyashov@gmail.com>
2020-05-21 11:50:21 +0700

    flock added

--- lazarusGUI/install.sh
+++ lazarusGUI/install.sh
@@ -9,12 +9,14 @@ sudo echo "@/home/pi/run.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart
 # must contain wants: postgresql.service
 # in the real raspbian it still starts before postgres and I do not have time to investigate it now
 # so let's put some sleep
-echo "sleep 30" >> /home/pi/run.sh
 :>/home/pi/lock.once
-echo "flock /home/pi/lock.once" >> /home/pi/run.sh
-echo "sudo systemctl restart lea-central-wash.service" >> /home/pi/run.sh
-echo "cd /home/pi/lea-central-wash/lazarusGUI" >> /home/pi/run.sh
-echo "./WashServerGUI" >> /home/pi/run.sh
+
+echo "sleep 30" >> /home/pi/run.sh
+echo "flock /home/pi/lock.once /home/pi/runonce.sh" >> /home/pi/run.sh
+echo "sudo systemctl restart lea-central-wash.service" >> /home/pi/runonce.sh
+echo "cd /home/pi/lea-central-wash/lazarusGUI" >> /home/pi/runonce.sh
+echo "./WashServerGUI" >> /home/pi/runonce.sh
 
 cd /home/pi
 sudo chmod 777 /home/pi/run.sh
+sudo chmod 777 /home/pi/runonce.sh
