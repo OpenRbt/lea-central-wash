068de08833639b37d26feccb237b7443b00274d2
DiaElectronics <roman.fedyashov@gmail.com>
2021-03-10 22:09:02 +0700

    fix

---
+++ cmd/storage/internal/app/relay.go
@@ -0,0 +1,32 @@
+package app
+
+func (a *app) RunProgram(id StationID, programID int64, preflight bool) (err error) {
+	cfg := RelayConfig{
+		TimeoutSec: relayTimeoutSec,
+	}
+	if programID > 0 {
+		program, err := a.repo.Programs(&programID)
+		if err != nil {
+			return err
+		}
+		if len(program) != 1 {
+			return ErrNotFound
+		}
+		if preflight {
+			station, err := a.repo.Station(id)
+			if err != nil {
+				return err
+			}
+			cfg.TimeoutSec = station.PreflightSec + 2
+		}
+
+		if preflight {
+			cfg.MotorSpeedPercent = int(program[0].PreflightMotorSpeedPercent)
+			cfg.Timings = append(cfg.Timings, program[0].PreflightRelays...)
+		} else {
+			cfg.MotorSpeedPercent = int(program[0].MotorSpeedPercent)
+			cfg.Timings = append(cfg.Timings, program[0].Relays...)
+		}
+	}
+	return a.hardware.RunProgram(int(id), cfg)
+}
--- cmd/storage/internal/app/storage.go
+++ cmd/storage/internal/app/storage.go
@@ -478,37 +478,6 @@ func (a *app) SetCardReaderConfig(cfg CardReaderConfig) error {
 	return a.repo.SetCardReaderConfig(cfg)
 }
 
-func (a *app) RunProgram(id StationID, programID int64, preflight bool) (err error) {
-	cfg := RelayConfig{
-		TimeoutSec: relayTimeoutSec,
-	}
-	if programID > 0 {
-		program, err := a.repo.Programs(&programID)
-		if err != nil {
-			return err
-		}
-		if len(program) != 1 {
-			return ErrNotFound
-		}
-		if preflight {
-			station, err := a.repo.Station(id)
-			if err != nil {
-				return err
-			}
-			cfg.TimeoutSec = station.PreflightSec + 2
-		}
-
-		if preflight {
-			cfg.MotorSpeedPercent = int(program[0].PreflightMotorSpeedPercent)
-			cfg.Timings = append(cfg.Timings, program[0].PreflightRelays...)
-		} else {
-			cfg.MotorSpeedPercent = int(program[0].MotorSpeedPercent)
-			cfg.Timings = append(cfg.Timings, program[0].Relays...)
-		}
-	}
-	return a.hardware.RunProgram(int(id), cfg)
-}
-
 func (a *app) Station(id StationID) (SetStation, error) {
 	return a.repo.Station(id)
 }
