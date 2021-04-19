0a9ff5308df92a97c82cda473f6acc59a98121f8
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-04-19 09:50:31 +0700

    atol instructions added

---
+++ install/ubuntu2004/7.1_install_atol_driver.sh
@@ -0,0 +1,4 @@
+echo download from here http://fs.atol.ru/SitePages/%D0%A6%D0%B5%D0%BD%D1%82%D1%80%20%D0%B7%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D0%BA%D0%B8.aspx
+echo direct link sample is
+echo http://fs.atol.ru/_layouts/15/atol.templates/Handlers/FileHandler.ashx?guid=700b2df4-42a8-4b0f-8ca1-ab0e2390cc56&webUrl=
+sleep 30
