59d2bba6b45fa58c6461a279ed821a9ec78e0a1d
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-05-22 21:45:55 +0700

    deletes golang tar after installation

--- install/ubuntu2004/2_install_golang.sh
+++ install/ubuntu2004/2_install_golang.sh
@@ -7,6 +7,7 @@ echo "Never use golang from repos =) It is too old"
 
 wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+rm go1.16.4.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
 
