235f6af5a77f278cabdb261e0421d149be405754
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-05-22 21:44:08 +0700

    small fix for go install

--- install/ubuntu2004/2_install_golang.sh
+++ install/ubuntu2004/2_install_golang.sh
@@ -1,11 +1,12 @@
+
 #!/bin/bash
 
 echo "Never use golang from repos =) It is too old"
 # sudo apt install golang
 
 
-wget -o ~/go.tar.gz https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf ~/go.tar.gz
+wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
 
