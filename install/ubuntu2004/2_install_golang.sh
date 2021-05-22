d7e0b312206ebc5f77c83c8ec5a218232f14e55e
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-05-22 21:30:25 +0700

    install scripts for ubuntu modified

--- install/ubuntu2004/2_install_golang.sh
+++ install/ubuntu2004/2_install_golang.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo apt install golang
+echo "Never use golang from repos =) It is too old"
+# sudo apt install golang
+
+
+wget -o ~/go.tar.gz https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf ~/go.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
+
--- install/ubuntu2004/4_build_project.sh
+++ install/ubuntu2004/4_build_project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/local/go/bin
 cd ../..
 ./build.sh
 cd install/ubuntu2004
