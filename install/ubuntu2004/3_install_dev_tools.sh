dd1e5dba091ed46cc7f9a4a51e17eab76b7e8d54
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-06-04 14:28:00 +0700

    git is now installed

--- install/ubuntu2004/3_install_dev_tools.sh
+++ install/ubuntu2004/3_install_dev_tools.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "installing GIT"
+sudo apt install -y git
+
 git clone https://github.com/Djarvur/go-swagger.git &&
     cd go-swagger/ && go install ./cmd/swagger/ && cd .. && rm -rf go-swagger/
 
