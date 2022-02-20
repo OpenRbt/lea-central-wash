e8b96bba145360021d0377e502024706f31d1db2
Roman Fedyashov <roman.fedyashov@gmail.com>
2022-02-20 14:27:03 +0700

    small fix

--- install/ubuntu2004/3_install_dev_tools.sh
+++ install/ubuntu2004/3_install_dev_tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "installing GIT"
 sudo apt install -y git
+export PATH=$PATH:/usr/local/go/bin
 
 git clone https://github.com/go-swagger/go-swagger.git &&
     cd go-swagger/ && go install ./cmd/swagger/ && cd .. && rm -rf go-swagger/
