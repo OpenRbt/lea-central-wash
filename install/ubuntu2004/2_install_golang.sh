855916b27d8e5dffdc948fec1fd9dafb6b0b3485
Roman Fedyashov <roman.fedyashov@gmail.com>
2022-02-20 14:04:46 +0700

    golang version updated

--- install/ubuntu2004/2_install_golang.sh
+++ install/ubuntu2004/2_install_golang.sh
@@ -5,9 +5,9 @@ echo "Never use golang from repos =) It is too old"
 # sudo apt install golang
 
 
-wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
-rm go1.16.4.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.17.7.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo  tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
+rm go1.17.7.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
 
