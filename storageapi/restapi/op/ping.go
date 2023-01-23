75e3a8243941c93d7b619cf8deecba1371f49824
Arne <shwanev.gzed@gmail.com>
2023-01-24 02:06:38 +0700

    add setBonuses and isAuthorized

--- storageapi/restapi/op/ping.go
+++ storageapi/restapi/op/ping.go
@@ -201,6 +201,12 @@ type PingOKBody struct {
 	// button ID
 	ButtonID int64 `json:"ButtonID,omitempty"`
 
+	// bonus amount
+	BonusAmount int64 `json:"bonusAmount,omitempty"`
+
+	// bonus system active
+	BonusSystemActive bool `json:"bonusSystemActive,omitempty"`
+
 	// last discount update
 	LastDiscountUpdate int64 `json:"lastDiscountUpdate,omitempty"`
 
@@ -214,6 +220,9 @@ type PingOKBody struct {
 	// service amount
 	// Required: true
 	ServiceAmount *int64 `json:"serviceAmount"`
+
+	// session ID
+	SessionID string `json:"sessionID,omitempty"`
 }
 
 // UnmarshalJSON unmarshals this object while disallowing additional properties from JSON
@@ -222,6 +231,12 @@ func (o *PingOKBody) UnmarshalJSON(data []byte) error {
 
 		// button ID
 		ButtonID int64 `json:"ButtonID,omitempty"`
+
+		// bonus amount
+		BonusAmount int64 `json:"bonusAmount,omitempty"`
+
+		// bonus system active
+		BonusSystemActive bool `json:"bonusSystemActive,omitempty"`
 
 		// last discount update
 		LastDiscountUpdate int64 `json:"lastDiscountUpdate,omitempty"`
@@ -236,6 +251,9 @@ func (o *PingOKBody) UnmarshalJSON(data []byte) error {
 		// service amount
 		// Required: true
 		ServiceAmount *int64 `json:"serviceAmount"`
+
+		// session ID
+		SessionID string `json:"sessionID,omitempty"`
 	}
 
 	dec := json.NewDecoder(bytes.NewReader(data))
@@ -245,10 +263,13 @@ func (o *PingOKBody) UnmarshalJSON(data []byte) error {
 	}
 
 	o.ButtonID = props.ButtonID
+	o.BonusAmount = props.BonusAmount
+	o.BonusSystemActive = props.BonusSystemActive
 	o.LastDiscountUpdate = props.LastDiscountUpdate
 	o.LastUpdate = props.LastUpdate
 	o.OpenStation = props.OpenStation
 	o.ServiceAmount = props.ServiceAmount
+	o.SessionID = props.SessionID
 	return nil
 }
 
