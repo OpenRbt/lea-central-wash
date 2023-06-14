cfd42b35a1a5f8896095bb4c06c01911685f4975
Kirill Skaryukin <kirill.skaryukin@dev.mobileup.ru>
2023-06-14 18:10:40 +0700

    updete go ver

--- install/ubuntu2004/5_add_service.sh
+++ install/ubuntu2004/5_add_service.sh
@@ -13,3 +13,8 @@ sed -i "s/{DIR}/$CUR_DIR/g" lea-central-wash.service
 sed -i "s/{USER}/$USER/g" lea-central-wash.service
 sudo cp lea-central-wash.service /etc/systemd/system/
 sudo systemctl enable lea-central-wash
+
+sed -i "s/{DIR}/$CUR_DIR/g" hal.service
+sed -i "s/{USER}/$USER/g" hal.service
+sudo cp hal.service /etc/systemd/system/
+sudo systemctl enable hal
